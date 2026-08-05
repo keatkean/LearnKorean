3b64ec8736bb7a41ab882191f5070e53f0d55dd6
Lee Keat Kean <keatkean@gmail.com>
2026-08-05 21:57:41 +0800

    Ci: add GitHub Actions workflow for GitHub Pages

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next';
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+if (isGithubActions) {
+  repo = process.env.GITHUB_REPOSITORY?.replace(/^.*?\//, '') || '';
+}
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isGithubActions && repo ? `/${repo}` : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
